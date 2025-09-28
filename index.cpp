b605615a2f4ec1805ffed61cd2c20b522862afdb
thinhvo1106-sudo <thinhvo1106@gmail.com>
2025-09-28 02:27:46 +0000

    testing

--- index.cpp
+++ index.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
-int main()
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() 
 {
-  cout<<"Hello world";
+  string day;
+  cout<<"Enter a day :" <<endl;
+  cin>>day;
+  transform(day.begin(), day.end(), day.begin(), ::tolower);
+
+  if(day=="monday" || day=="tuesday" || day=="wednesday" || day=="thursday"||day==
+  "friday")
+  {
+      cout<<"It's Weekday!";
+  }
+  else
+  if(day=="saturday"|| day=="sunday")
+  {
+      cout<<"It's Weekend!";
+  }
+  
 }
